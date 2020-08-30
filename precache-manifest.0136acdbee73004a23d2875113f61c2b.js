3795ab7455ac8102bbedf4d35055b3618410e1c0
FifteenFifty <matt.bissenden@gmail.com>
2020-08-30 13:35:20 +0100

    Updates

---
+++ precache-manifest.0136acdbee73004a23d2875113f61c2b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4bd1313f16c060b08a7",
+    "url": "/prog/static/js/app.919db510.chunk.js"
+  },
+  {
+    "revision": "a44795bb381e34573f1c",
+    "url": "/prog/static/js/runtime~app.5e65f23f.js"
+  },
+  {
+    "revision": "af1e2f69f8e568a507c1",
+    "url": "/prog/static/js/2.7029d554.chunk.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/prog/static/media/flags.99f63ae7.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/prog/./fonts/icons.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/prog/./fonts/icons.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/prog/./fonts/icons.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/prog/static/media/icons.62d9dae4.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/prog/static/media/outline-icons.9c4845b4.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/prog/./fonts/brand-icons.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/prog/./fonts/brand-icons.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/prog/./fonts/brand-icons.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/prog/./fonts/brand-icons.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/prog/static/media/brand-icons.6729d297.svg"
+  },
+  {
+    "revision": "d310d1533433c41cbd2efe250d51f761",
+    "url": "/prog/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/prog/serve.json"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/prog/expo-service-worker.js"
+  },
+  {
+    "revision": "584de56c02b88134305e5ef89303aad4",
+    "url": "/prog/register-service-worker.js"
+  },
+  {
+    "revision": "43020d7b0428981c20558edc82e3c5eb",
+    "url": "/prog/static/js/2.7029d554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/prog/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/prog/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/prog/favicon.ico"
+  },
+  {
+    "revision": "8ce46cd6e266f633193db668201edb5b",
+    "url": "/prog/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/prog/expo-service-worker.js",{scope:"/prog/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
